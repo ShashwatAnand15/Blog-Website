47335fd8b9752603e8e44efb5f7b0fe91f1c6491
Shashwat Anand <shashwatanand15@gmail.com>
2023-01-25 17:47:25 +0530

    Added Posts to mongoDB

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
+
 
 const app = express();
 
@@ -29,8 +29,15 @@ const Post = mongoose.model("Post",postSchema);
 
 
 app.get("/" , function(req , res) {
-  res.render("home" , {homeData : homeStartingContent,
-  posts:posts});
+  
+  Post.find({} , function(err,posts){
+    if(!err){
+      res.render("home" , {
+        homeData: homeStartingContent,
+         posts: posts
+      });
+    }
+  })
 });
 
 app.get("/about" , function(req , res){
@@ -47,16 +54,15 @@ app.get("/compose" ,function(req , res) {
 // we can use this to create multipe pages without having actual routes
 // it is given in express package
 app.get('/posts/:postId', function(req, res) {
-  const requestedTitle = _.lowerCase(req.params.postId);
+  const requestedPostId = req.params.postId;
 
-  posts.forEach(function(post){
-    const savedTitle = _.lowerCase(post.title);
-    console.log(savedTitle);
-    if(requestedTitle === savedTitle){
-      res.render("post" , {postTitle : post.title , postContent : post.content});
-    }
+  Post.findOne({_id:requestedPostId} , function(err,post){
+    res.render("post", {
+      postTitle: post.title,
+      postContent: post.content
+    });
   });
-})
+});
 
 
 app.post("/compose" , function(req,res){
@@ -66,8 +72,11 @@ app.post("/compose" , function(req,res){
     content: req.body.postBody
   });
 
-  post.save();
-  res.redirect("/");
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 })
 
 
